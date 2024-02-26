8630ee90c4a2b4c96741f6f8945539639e6c6658
Jyotsna03 <jyotsnabh03@gmail.com>
2024-02-26 10:55:58 +0530

    login successful

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter,Routes,Route} from 'react-router-dom';
 import Login from './Components/Login';
 import { Home } from './Components/Home';
-
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   
     return (
       <div>
+        <ToastContainer theme ='colored'> </ToastContainer>
         <BrowserRouter>
         <Routes>
           <Route path = '/' Component = {Login}/> 
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,30 +1,45 @@
-import e from 'express';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {toast } from "react-toastify";
 
 const Login = () => {
 
-    const[id,idchange] = useState("");
-    const[password,passwordchange] = useState("");
-    const handlesubmit=(e)=> {
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
         e.preventDefault();
+        let regobj = {Email, password };
+        //console.log(regobj);
+
+        fetch("http://localhost:3001/",{
+            method: "POST",
+            headers: {'content-type':'application/json'},
+            body:JSON.stringify(regobj)
+        }).then((res) =>{
+            toast.success('Registered Sucessfully')
+            navigate('/home');
+        }).catch((err) => {
+            toast.error('Failed :' +err.message);
+        });
 
     }
+
     return (
-        <div className='wrapper' onSubmit={handlesubmit}>
-            <form action=''>
+        <div className='wrapper'>
+            <form onSubmit={handleSubmit}>
                 <h1>Login</h1>
                 <div className='input-box'>
-                    <input type='text' placeholder='Username' required/>
-                    <input value ={id} onChange={e=>idchange(e.target.value)} className='form'></input>
+                    <input type='text' placeholder='Username' value={id} onChange={e => setEmail(e.target.value)} required />
                 </div>
                 <div className='input-box'>
-                    <input type='password' placeholder='Password' required/>
-                    <input value ={password} onChange={e=>passwordchange(e.target.value)} className='passwd'></input>
-                
+                    <input type='password' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} required />
                 </div>
-<button type="submit">Login</button>
+                <button type="submit">Login</button>
             </form>
-            </div>
+        </div>
     );
 };
 
