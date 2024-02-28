707519c51d68d99339ed6632ec65d395be7a4441
Jyotsna03 <jyotsnabh03@gmail.com>
2024-02-28 23:57:23 +0530

    Product Info

--- src/Components/prodinfo.js
+++ src/Components/prodinfo.js
@@ -1,7 +1,21 @@
-import React from 'react'
+const ProdInfo = [
+  {
+    ID: 1,
+    Name: 'Lenovo IdeaPad',
+    Image: './images/img1.jpg',
+    Desc: 'Everyday laptops for first-time users. High-performance laptops for serious gamers. Convertible PCs for those who like their entertainment on the go. Embracing form, function, style, and fun, each Ideapad delivers responsive performance you can count on.',
+    Price: '50,000',
+    memory: {
+      name : "Ryzen 7000",
+      value: "8gb"
+    },
+    SSD: {
+      name: "SSD",
+      value : "256 GB"
+    },
+    screen : "Anti-Glare, Non-Touch",
+    color: "Black"
 
-export const prodinfo = () => {
-  return (
-    <div> abc</div>
-  )
-}
+    
+  }
+]
